b85230c22a8df65787f5311d2eb093e862fd90fc
comp500 <comp500@users.noreply.github.com>
2021-11-04 22:17:40 +0000

    Change generated game directory

--- app/src/main/java/link/infra/packwiz/vanillainstaller/PathUtils.java
+++ app/src/main/java/link/infra/packwiz/vanillainstaller/PathUtils.java
@@ -3,17 +3,35 @@ package link.infra.packwiz.vanillainstaller;
 import net.fabricmc.installer.util.Utils;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Locale;
 
 public class PathUtils {
 	private PathUtils() {}
 
+	private static final String os = System.getProperty("os.name").toLowerCase();
+
 	public static Path getMinecraftPath() {
 		return Utils.findDefaultInstallDir();
 	}
 
+	public static Path getPackwizUserPath() {
+		if (os.contains("win")){
+			return Paths.get(System.getenv("APPDATA")).resolve("packwiz");
+		} else if (os.contains("mac")) {
+			return Paths.get(System.getProperty("user.home")).resolve("Library").resolve("Application Support").resolve("packwiz");
+		} else {
+			// TODO: a better location on linux?
+			return Paths.get(System.getProperty("user.home")).resolve("packwiz");
+		}
+	}
+
+	public static Path getPackwizInstancesPath() {
+		return getPackwizUserPath().resolve("instances");
+	}
+
 	public static Path getGeneratedPath(String packName) {
-		return getMinecraftPath().resolve("packwiz-packs").resolve(slugify(packName));
+		return getPackwizInstancesPath().resolve(slugify(packName));
 	}
 
 	private static String slugify(String packName) {
